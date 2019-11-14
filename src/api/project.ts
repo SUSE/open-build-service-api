7fed3ac03951d308e537cc8dfdab20caf1a4e255
Dan Čermák <dcermak@suse.com>
2019-11-14 14:51:05 +0100

    Implement conversion from Api objects to OBS API types

--- src/api/project.ts
+++ src/api/project.ts
@@ -1,12 +1,20 @@
-import { Connection } from "../connection";
-import { Project } from "../obs";
-import { Group, User, groupFromApi, userFromApi } from "../user";
-import { extractElementAsArray, extractElementIfPresent } from "../util";
 import * as assert from "assert";
+import { Connection, RequestMethod } from "../connection";
+import { StatusReply, statusReplyFromApi } from "../error";
+import * as project from "../project";
+import * as user from "../user";
+import {
+  extractElementAsArray,
+  extractElementIfPresent,
+  setPropertyIfDefined,
+  setPropertyOnCondition,
+  deleteUndefinedMembers
+} from "../util";
 import { BaseProject, BaseRepository } from "./base_types";
+import * as flag from "./flag";
 
-
-function parseRepositoryFromApi(data: {
+/** Layout of a repository element as converted from OBS' API via xml2js */
+interface BaseRepositoryApiReply {
   $: {
     name: string;
     rebuild?: string;
@@ -14,54 +22,242 @@ function parseRepositoryFromApi(data: {
     linkedbuild?: string;
   };
   arch: string[];
-  releasetarget: {
-    $: { project: string; repository: string; trigger?: string };
-  };
-  path: { $: { repository: string; project: string } };
-}): BaseRepository {
+  releasetarget: project.ReleaseTargetApiReply[];
+  path: project.PathApiReply[];
+}
+
+function baseRepositoryFromApi(data: BaseRepositoryApiReply): BaseRepository {
   return {
-    arch: extractElementAsArray(data, "arch", {
-      construct: (data: Project.Arch): Project.Arch => data
-    }),
-    block: extractElementIfPresent<Project.BlockMode>(data.$, "block"),
-    linkedbuild: extractElementIfPresent<Project.LinkedBuildMode>(
+    arch: extractElementAsArray(data, "arch"),
+    block: extractElementIfPresent<project.BlockMode>(data.$, "block"),
+    linkedbuild: extractElementIfPresent<project.LinkedBuildMode>(
       data.$,
       "linkedbuild"
     ),
     name: data.$.name,
     path: extractElementAsArray(data, "path", {
-      type: Project.Path
+      construct: project.pathFromApi
     }),
-    rebuild: extractElementIfPresent<Project.RebuildMode>(data.$, "rebuild"),
+    rebuild: extractElementIfPresent<project.RebuildMode>(data.$, "rebuild"),
     releasetarget: extractElementAsArray(data, "releasetarget", {
-      type: Project.ReleaseTarget
+      construct: project.releaseTargetFromApi
     })
   };
 }
 
+function baseRepositoryToApi(repo: BaseRepository): BaseRepositoryApiReply {
+  return {
+    $: {
+      block: repo.block,
+      linkedbuild: repo.linkedbuild,
+      name: repo.name,
+      rebuild: repo.rebuild
+    },
+    arch: repo.arch,
+    path: repo.path.map(pth => project.pathToApi(pth)),
+    releasetarget: repo.releasetarget.map(relTgt =>
+      project.releaseTargetToApi(relTgt)
+    )
+  };
+}
+
+/**
+ * Representation of a project on OBS
+ *
+ * This interface is very closely mimicking the way how the data are expected by
+ * OBS' API and is thus relatively ugly to use in practice (e.g. the
+ * [[debugInfo]], [[useForBuild]], etc. settings are configured externally from
+ * the actual repository). This is merely an intermediate state that is used to
+ * convert it to a more useful form later.
+ */
 export interface Project extends BaseProject {
-  // building enabled/disabled for certain repositories
-  readonly build?: Flag;
-  // publishing of certain repositories enabled or disabled?
-  readonly publish?: Flag;
-  // useforbuild (build results from packages will be used to build other
-  // packages in contrast to external dependencies) disabled?
-  readonly useforbuild?: Flag;
-  // debuginfo generation settings
-  readonly debuginfo?: Flag;
+  /** building enabled/disabled for certain repositories */
+  readonly build?: flag.Flag;
+
+  /** publishing of certain repositories enabled or disabled? */
+  readonly publish?: flag.Flag;
+
+  /**
+   * useforbuild (build results from packages will be used to build other
+   * packages in contrast to external dependencies) disabled?
+   */
+  readonly useForBuild?: flag.Flag;
+
+  /** debuginfo generation settings */
+  readonly debugInfo?: flag.Flag;
 
   // <binarydownload> field is used for things...
   // No idea what for and according to Adrian it should be hidden better, so here we go ;-)
-  // readonly binarydownload?: Flag;
+  // readonly binarydownload?: flag.Flag;
 
-  // repositories for this project
+  /** repositories for this project */
   readonly repository: BaseRepository[];
 }
-// return the route to GET or PUT the project's _meta
+
+/**
+ * @return the route to GET or PUT the project's _meta
+ */
 function metaRoute(name: string): string {
   return "/source/".concat(name, "/_meta");
 }
 
+/** Layout of the project configuration as received from OBS' API */
+interface ProjectApiReply {
+  project: {
+    $: { name: string };
+    access?: flag.SimpleFlagApiReply;
+    build?: flag.FlagApiReply;
+    created?: string;
+    debuginfo?: flag.FlagApiReply;
+    description: string;
+    group?: user.GroupApiReply[];
+    kind?: project.Kind;
+    link?: project.LinkApiReply[];
+    lock?: flag.SimpleFlagApiReply;
+    mountproject?: string;
+    person?: user.UserApiReply[];
+    publish?: flag.FlagApiReply;
+    repository?: BaseRepositoryApiReply[];
+    sourceaccess?: flag.SimpleFlagApiReply;
+    title: string;
+    updated?: string;
+    url?: string;
+    useforbuild?: flag.FlagApiReply;
+  };
+}
+
+function projectFromApi(data: ProjectApiReply): Project {
+  const lock = extractElementIfPresent(data.project, "lock", {
+    construct: flag.simpleFlagToBoolean
+  });
+
+  const access = extractElementIfPresent<boolean>(data.project, "access", {
+    construct: flag.simpleFlagToBoolean
+  });
+  const sourceAccessElem = extractElementIfPresent<boolean>(
+    data.project,
+    "sourceaccess",
+    { construct: flag.simpleFlagToBoolean }
+  );
+
+  const res = {
+    access,
+    build: extractElementIfPresent<flag.Flag>(data.project, "build", {
+      construct: flag.flagFromApi
+    }),
+    created: extractElementIfPresent<string>(data.project, "created"),
+    debugInfo: extractElementIfPresent<flag.Flag>(data.project, "debuginfo", {
+      construct: flag.flagFromApi
+    }),
+    description: data.project.description,
+    group: extractElementAsArray<user.Group>(data.project, "group", {
+      construct: user.groupFromApi
+    }),
+
+    kind: extractElementIfPresent<project.Kind>(data.project, "kind"),
+    link: extractElementAsArray<project.Link>(data.project, "link", {
+      construct: project.linkFromApi
+    }),
+    lock,
+    mountProject: extractElementIfPresent<string>(data.project, "mountproject"),
+    name: data.project.$.name,
+    person: extractElementAsArray<user.User>(data.project, "person", {
+      construct: user.userFromApi
+    }),
+    publish: extractElementIfPresent<flag.Flag>(data.project, "publish", {
+      construct: flag.flagFromApi
+    }),
+    repository: extractElementAsArray(data.project, "repository", {
+      construct: baseRepositoryFromApi
+    }),
+    sourceAccess: sourceAccessElem,
+    title: data.project.title,
+    updated: extractElementIfPresent<string>(data.project, "updated"),
+    url: extractElementIfPresent<string>(data.project, "url"),
+    useForBuild: extractElementIfPresent<flag.Flag>(
+      data.project,
+      "useforbuild",
+      {
+        construct: flag.flagFromApi
+      }
+    )
+  };
+  deleteUndefinedMembers(res);
+  return res;
+}
+
+function projectToApi(proj: Project): ProjectApiReply {
+  const projApi: ProjectApiReply = {
+    project: {
+      $: { name: proj.name },
+      title: proj.title,
+      description: proj.description
+    }
+  };
+  setPropertyIfDefined(
+    projApi.project,
+    "access",
+    flag.booleanToSimpleFlag(proj.access)
+  );
+  setPropertyIfDefined(projApi.project, "build", flag.flagToApi(proj.build));
+  setPropertyIfDefined(projApi.project, "created", proj.created);
+  setPropertyIfDefined(
+    projApi.project,
+    "debuginfo",
+    flag.flagToApi(proj.debugInfo)
+  );
+  setPropertyIfDefined(
+    projApi.project,
+    "group",
+    proj.group?.map(grp => user.groupToApi(grp))
+  );
+  setPropertyIfDefined(projApi.project, "kind", proj.kind);
+  setPropertyIfDefined(
+    projApi.project,
+    "link",
+    proj.link?.map(lnk => project.linkToApi(lnk))
+  );
+  setPropertyIfDefined(
+    projApi.project,
+    "lock",
+    flag.booleanToSimpleFlag(proj.lock)
+  );
+  setPropertyIfDefined(projApi.project, "mountproject", proj.mountProject);
+  setPropertyIfDefined(
+    projApi.project,
+    "person",
+    proj.person?.map(pers => user.userToApi(pers))
+  );
+  setPropertyIfDefined(
+    projApi.project,
+    "publish",
+    flag.flagToApi(proj.publish)
+  );
+  setPropertyOnCondition(
+    projApi.project,
+    "repository",
+    proj.repository.map(repo => baseRepositoryToApi(repo)),
+    proj.repository.length > 0
+  );
+  setPropertyIfDefined(
+    projApi.project,
+    "sourceaccess",
+    flag.booleanToSimpleFlag(proj.sourceAccess)
+  );
+  setPropertyIfDefined(projApi.project, "updated", proj.updated);
+  setPropertyIfDefined(projApi.project, "url", proj.url);
+  setPropertyIfDefined(
+    projApi.project,
+    "useforbuild",
+    flag.flagToApi(proj.useForBuild)
+  );
+
+  return projApi;
+}
+
+/**
+ *
+ */
 export async function getProject(
   conn: Connection,
   projName: string
@@ -71,66 +267,16 @@ export async function getProject(
     res.project.$.name === projName,
     "Expected the received project name and the sent project name to be equal"
   );
+  return projectFromApi(res);
+}
 
-  const lockElem:
-    | { enable?: {}; disable?: {} }
-    | undefined = extractElementIfPresent(res.project, "lock");
-
-  const accessElem = extractElementIfPresent<boolean>(res.project, "access", {
-    construct: simpleFlagToBoolean
+export async function modifyOrCreateProject(
+  conn: Connection,
+  proj: Project
+): Promise<StatusReply> {
+  const resp = await conn.makeApiCall("/source/".concat(proj.name, "/_meta"), {
+    method: RequestMethod.PUT,
+    payload: projectToApi(proj)
   });
-  const sourceAccessElem = extractElementIfPresent<boolean>(
-    res.project,
-    "sourceaccess",
-    { construct: simpleFlagToBoolean }
-  );
-
-  const proj = {
-    access: accessElem,
-    build: extractElementIfPresent<Flag>(res.project, "build", {
-      construct: flagFromApi
-    }),
-    created: extractElementIfPresent<string>(res.project, "created"),
-    debuginfo: extractElementIfPresent<Flag>(res.project, "debuginfo", {
-      construct: flagFromApi
-    }),
-    description: res.project.description,
-    group: extractElementAsArray<Group>(res.project, "group", {
-      construct: groupFromApi
-    }),
-
-    kind: extractElementIfPresent<Project.Kind>(res.project, "kind"),
-    link: extractElementAsArray<Project.Link>(res.project, "link", {
-      type: Project.Link
-    }),
-    lock: lockElem === undefined ? false : simpleFlagToBoolean(lockElem),
-    mountproject: extractElementIfPresent<string>(res.project, "mountproject"),
-    name: projName,
-    person: extractElementAsArray<User>(res.project, "person", {
-      construct: userFromApi
-    }),
-    publish: extractElementIfPresent<Flag>(res.project, "publish", {
-      construct: flagFromApi
-    }),
-    repository: extractElementAsArray(res.project, "repository", {
-      construct: parseRepositoryFromApi
-    }),
-    sourceAccess: sourceAccessElem,
-    title: res.project.title,
-    updated: extractElementIfPresent<string>(res.project, "updated"),
-    url: extractElementIfPresent<string>(res.project, "url"),
-    useforbuild: extractElementIfPresent<Flag>(res.project, "useforbuild", {
-      construct: flagFromApi
-    })
-  };
-
-  return proj;
-}
-
-export async function create(conn: Connection, proj: Project): Promise<any> {
-  return conn.makeApiCall("/source/".concat(proj.name, "/_meta"), "PUT");
-}
-
-export async function getMeta(conn: Connection, proj: Project): Promise<any> {
-  return conn.makeApiCall(metaRoute(proj.name));
+  return statusReplyFromApi(resp);
 }
