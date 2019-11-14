73fb5e6ca873cf936903977c29382bdecce8855b
Dan Čermák <dcermak@suse.com>
2019-11-14 14:41:50 +0100

    Make array properties of BaseRepository & BaseProject optional

--- src/api/base_types.ts
+++ src/api/base_types.ts
@@ -1,25 +1,29 @@
-// Collection of base types shared by the API module and the user facing modules
+/**
+ * Collection of base types shared by the API module and the user facing modules
+ */
 
-import { Project } from "../obs";
+import * as project from "../project";
 import { Group, User } from "../user";
 
-// Interface of a repository (= build target) on OBS
+/** Interface of a repository (= build target) on OBS */
 export interface BaseRepository {
+  /** Name of this repository */
   name: string;
-  rebuild?: Project.RebuildMode;
-  block?: Project.BlockMode;
-  linkedbuild?: Project.LinkedBuildMode;
+  /** Mode in which the project */
+  rebuild?: project.RebuildMode;
+  block?: project.BlockMode;
+  linkedbuild?: project.LinkedBuildMode;
 
-  // architecture of this repository
-  arch: Project.Arch[];
+  /** architectures which this repository builds for */
+  arch: project.Arch[];
 
-  // repositories that should be released
-  releasetarget: Project.ReleaseTarget[];
+  /** repositories that should be released */
+  releasetarget: project.ReleaseTarget[];
 
-  // Array of included repositories from other projects
-  path: Project.Path[];
+  /** Array of included repositories from other projects */
+  path: project.Path[];
 
-  hostsystem?: Project.HostSystem;
+  hostsystem?: project.HostSystem;
 }
 
 export interface BaseProject {
@@ -31,20 +35,20 @@ export interface BaseProject {
   url?: string;
 
   mountProject?: string;
-  kind?: Project.Kind;
-  // list of users and their roles belonging to this project
-  person: User[];
+  kind?: project.Kind;
+  /** list of users and their roles belonging to this project */
+  person?: User[];
 
-  link: Project.Link[];
-  // list of groups and their roles belonging to this project
-  group: Group[];
+  link?: project.Link[];
+  /** list of groups and their roles belonging to this project */
+  group?: Group[];
 
   // Is this project locked from rebuilding (used for maintenance project)
-  lock: boolean;
+  lock?: boolean;
 
   // if set to false, then this hides the entire project from being visible
-  access: boolean | undefined;
+  access?: boolean;
 
   // if set to false, then this hides source in packages and build logs and stuff
-  sourceAccess: boolean | undefined;
+  sourceAccess?: boolean;
 }
