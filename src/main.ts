f69937f81e56e954d31b602a7bdb39a5ace0d719
Dan Čermák <dcermak@suse.com>
2019-11-14 16:52:07 +0100

    Export everything from the project module

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-export { Project } from "./project";
+export * from "./project";
 export { Group, User } from "./user";
 export { Connection, normalizeUrl } from "./connection";
 export { Account, addAccountToOscrc, readAccountsFromOscrc } from "./account";
