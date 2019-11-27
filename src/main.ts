0c4d8add7749a2c40a65bd6496c6ddb9eca2a291
Dan Čermák <dcermak@suse.com>
2019-11-27 14:21:21 +0100

    Rework exported symbols

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
+export { Account, addAccountToOscrc, readAccountsFromOscrc } from "./account";
+export { Arch, BaseRepository, Path } from "./api/base-types";
+export * from "./api/project-meta";
+export { Connection, normalizeUrl } from "./connection";
+export * from "./package";
 export * from "./project";
 export { Group, User } from "./user";
-export { Connection, normalizeUrl } from "./connection";
-export { Account, addAccountToOscrc, readAccountsFromOscrc } from "./account";
