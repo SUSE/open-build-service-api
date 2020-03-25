2f84f10e11fbfe2ab2705cc7d3df50c1a87329d5
Dan Čermák <dcermak@suse.com>
2020-03-25 21:25:52 +0100

    Add utilities to work with sets
    Apparently js doesn't have functions to calculate set intersections and
    differences, so we roll our own.

---
+++ src/set-utils.ts
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2020 SUSE LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/** Returns all elements in `setA` that are not in `setB` */
+export function setDifference<T>(setA: Set<T>, setB: Set<T>): Set<T> {
+  const diff: Set<T> = new Set();
+  for (const elem of setA) {
+    if (!setB.has(elem)) {
+      diff.add(elem);
+    }
+  }
+  return diff;
+}
+
+/** Returns all elements that are in `setA` and in `setB` */
+export function setIntersection<T>(setA: Set<T>, setB: Set<T>): Set<T> {
+  const intersection: Set<T> = new Set();
+  for (const elem of setA) {
+    if (setB.has(elem)) {
+      intersection.add(elem);
+    }
+  }
+  return intersection;
+}
