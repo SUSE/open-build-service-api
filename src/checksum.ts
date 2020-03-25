2a2413db1ce706e7942cf7523edd8e573598b5f4
Dan Čermák <dcermak@suse.com>
2020-03-25 21:19:15 +0100

    Add module to calculate checksums of files & buffers

---
+++ src/checksum.ts
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2020 SUSE LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import { createHash } from "crypto";
+import { promises as fsPromises } from "fs";
+import { pathExists, PathType } from "./util";
+
+/** Hash functions supported by the functions from this module */
+export type SupportedHashfunction = "md5" | "sha256";
+
+/**
+ * Return the hash digest of the Buffer `contents` with the given
+ * `hashFunction` encoded as hex.
+ */
+export function calculateHash(
+  contents: Buffer,
+  hashFunction: SupportedHashfunction
+): string {
+  const hash = createHash(hashFunction);
+  hash.update(contents);
+  return hash.digest("hex");
+}
+
+const CHUNKSIZE = 4096;
+
+/**
+ * Return the hash of the file contents with the provided hash function.
+ *
+ * @return The hex digest of the hash of the raw file contents or undefined if
+ *     the file does not exist.
+ */
+export async function calculateFileHash(
+  path: string,
+  hashFunction: SupportedHashfunction
+): Promise<string | undefined> {
+  if (!(await pathExists(path, PathType.File))) {
+    return undefined;
+  }
+  const hash = createHash(hashFunction);
+  const buff = new Uint8Array(CHUNKSIZE);
+  const fd = await fsPromises.open(path, "r");
+
+  try {
+    let readRes: { bytesRead: number; buffer: Uint8Array };
+
+    do {
+      readRes = await fd.read(buff, 0, CHUNKSIZE);
+      hash.update(new DataView(readRes.buffer.buffer, 0, readRes.bytesRead));
+    } while (readRes.bytesRead > 0);
+
+    return hash.digest("hex");
+  } finally {
+    await fd.close();
+  }
+}
---
+++ test/checksum.test.ts
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2020 SUSE LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import mockFs = require("mock-fs");
+
+import { afterEach, beforeEach, describe, it } from "mocha";
+import { calculateFileHash, calculateHash } from "../src/checksum";
+
+describe("hashes", () => {
+  describe("#calculateFileHash", () => {
+    let longFileContents: string = "";
+    while (longFileContents.length < 10000) {
+      longFileContents = longFileContents.concat(Math.random().toString());
+    }
+
+    const largeFile = Buffer.from(longFileContents);
+
+    beforeEach(() =>
+      mockFs({
+        large: largeFile,
+        test: `test
+`
+      })
+    );
+    afterEach(() => mockFs.restore());
+
+    it("calculates the hash of a file larger than the chunksize correctly", async () => {
+      await calculateFileHash(
+        "large",
+        "md5"
+      ).should.be.fulfilled.and.eventually.deep.equal(
+        calculateHash(largeFile, "md5")
+      );
+    });
+
+    it("calculates sha256sums correctly", async () => {
+      await calculateFileHash(
+        "test",
+        "sha256"
+      ).should.be.fulfilled.and.eventually.deep.equal(
+        // echo "test"|sha256sum:
+        "f2ca1bb6c7e907d06dafe4687e579fce76b37e4e93b7605022da52e6ccc26fd2"
+      );
+    });
+
+    it("returns undefined when the file doesn't exist", async () => {
+      await calculateFileHash("does_not_exist", "md5").should.be.fulfilled.and
+        .eventually.be.undefined;
+    });
+  });
+});
