ac86df170f3e1daee9f5267e905ae8fb38539a75
Dan Čermák <dcermak@suse.com>
2022-04-13 14:38:40 +0200

    Bump copyright year

--- test/package.test.ts
+++ test/package.test.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2020 SUSE LLC
+ * Copyright (c) 2022 SUSE LLC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
--- utilities/xml2json.js
+++ utilities/xml2json.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /**
- * Copyright (c) 2020 SUSE LLC
+ * Copyright (c) 2020-2022 SUSE LLC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
